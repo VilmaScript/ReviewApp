2b75a03c1b3f508a81b74a72fb3cfed5d5ea9e8c
abara vivian <vilmalamberts10@gmail.com>
2024-01-18 00:55:10 +0100

    Added some new changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/ReviewApp/', // Replace with your actual repository name
+  build: {
+    outDir: 'docs', // or any other directory you prefer
+  },
+});
+
+
+
